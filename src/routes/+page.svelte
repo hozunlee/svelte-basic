49c2a99b688b9ec577ce6d74428270b557151884
hozunlee <ho2yahh@gmail.com>
2023-01-27 15:10:03 +0900

    Reactivity : Declarations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 	// your script goes here
 	let count = 0;
 
+	// 여러 번 참조하거나, 다른 반응형 값에 따라 달라지는 값이 있을 때 사용
+	$: doubled = count * 2;
+
 	const increment = () => {
 		count += 1;
 	};
@@ -12,6 +15,8 @@
 		Clicked {count}
 		{count <= 1 ? 'time' : 'times'}
 	</button>
+	<p>{count} doubled is {doubled}</p>
+	<p>{count} doubled is {count * 2}</p>
 </div>
 
 <!-- markup (zero or more items) goes here -->
