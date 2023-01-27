f81e597b494413bbe2f0906ece50fcd7f67995d8
hozunlee <ho2yahh@gmail.com>
2023-01-27 15:15:40 +0900

    Reactivity : Statements 반응형 state관리

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,18 @@
 	// your script goes here
 	let count = 0;
 
-	// 여러 번 참조하거나, 다른 반응형 값에 따라 달라지는 값이 있을 때 사용
-	$: doubled = count * 2;
+	// 첫 실행 시에만 동작한다.
+	console.log(count);
+
+	// 반응형 밸류는 count가 바뀔 때마다 실행한다. like useEffect
+	$: {
+		console.log(`the count is ${count}`);
+	}
+
+	$: if (count >= 10) {
+		alert('reset');
+		count = 0;
+	}
 
 	const increment = () => {
 		count += 1;
@@ -15,7 +25,6 @@
 		Clicked {count}
 		{count <= 1 ? 'time' : 'times'}
 	</button>
-	<p>{count} doubled is {doubled}</p>
 	<p>{count} doubled is {count * 2}</p>
 </div>
 
