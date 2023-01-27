98e21d6b3f3dabe2b122363363b649d38a614fa9
hozunlee <ho2yahh@gmail.com>
2023-01-27 14:40:33 +0900

    part1 : nested components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
+<script>
+	import Nested from './Nested.svelte';
+</script>
+
 <div>
 	<p>This is a test</p>
+	<Nested />
 </div>
 
 <style>
 	div {
 		display: flex;
+		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 	}
