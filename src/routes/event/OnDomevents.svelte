06dbc4ee21d7969942794436f557ef96f889ba7b
hozunlee <ho2yahh@gmail.com>
2023-01-30 15:42:12 +0900

    refactor : inline handlers

--- src/routes/event/OnDomevents.svelte
+++ src/routes/event/OnDomevents.svelte
@@ -7,6 +7,9 @@
 	};
 </script>
 
+<!-- 
+  inline handlers svelte는 이렇게해도 성능상의 이슈가 전혀 없다!
+  <div on:mousemove={e =>m = {x: e.clientX, y: e.clientY}}> -->
 <div on:mousemove={handleMousemove}>the mouse position is {m.x} x {m.y}</div>
 
 <style>
