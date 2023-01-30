5d61584624d5e20c6e59c1635b7d7d3c24d74eb1
hozunlee <ho2yahh@gmail.com>
2023-01-30 15:37:35 +0900

    Event : on:DomEvent

---
+++ src/routes/event/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import OnDomevents from './OnDomevents.svelte';
+</script>
+
+<h1>event part</h1>
+
+<div class="container">
+	<OnDomevents />
+</div>
+
+<style>
+	.container {
+		height: 100vh;
+		display: flex;
+	}
+</style>
